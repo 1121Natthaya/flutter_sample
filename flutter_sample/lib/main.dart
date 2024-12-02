2d978d73e218ef4a9600e3dd5657309050ab0a26
1121Natthaya <3651051541121@rmutr.ac.th>
2024-12-02 11:42:32 +0700

    second commit

--- flutter_sample/lib/main.dart
+++ flutter_sample/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/my_container.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,13 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      show
-      //title:), 
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title:"My Flutter Sample", 
+      home: MyContainer(),
     );
   }
 }
